f04ef2b50f5a672703b884dcb7ac211b088a870a
Vitor <vitormr45@gmail.com>
2020-03-07 20:23:03 -0300

    Front-end done

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
+import logo from '../../assets/img/logo.png';
 
-// import { Container } from './styles';
+import { Container, Textarea, Form, SubmitButton } from './styles';
 
-export default function Main() {
-  return <h1>Main</h1>;
+export default class Main extends Component {
+  state = {
+    name: '',
+    email: '',
+    text: '',
+  };
+
+  handleNameChange = event => {
+    this.setState({ name: event.target.value });
+  };
+
+  handleEmailChange = event => {
+    this.setState({ email: event.target.value });
+  };
+
+  handleTextChange = event => {
+    this.setState({ text: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    console.log(this.state.text);
+  };
+
+  render() {
+    const { name, email, text } = this.state;
+
+    return (
+      <>
+        <Container>
+          <img src={logo} alt="CPS" />
+          <h1>Bem vindo(a) a Cloud Predictive Solution</h1>
+          <hr />
+          <p>EM BREVE UMA NOVA CPS</p>
+          <Form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              placeholder="Nome"
+              value={name}
+              onChange={this.handleNameChange}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={this.handleEmailChange}
+            />
+            <Textarea
+              placeholder="Sua mensagem"
+              value={text}
+              onChange={this.handleTextChange}
+            />
+
+            <SubmitButton>Enviar</SubmitButton>
+          </Form>
+        </Container>
+      </>
+    );
+  }
 }
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 700px;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  padding: 30px;
+  margin: 80px auto;
+
+  img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  h1 {
+    font-size: 20px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 10px;
+  }
+
+  p {
+    font-size: 30px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin-top: 10px;
+  }
+`;
+
+export const Textarea = styled.textarea`
+  border: 1px solid #eee;
+  padding: 10px 15px;
+  border-radius: 4px;
+  font-size: 16px;
+  margin-bottom: 20px;
+  height: 130px;
+`;
+
+export const Form = styled.form`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+
+  input {
+    border: 1px solid #eee;
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 16px;
+    margin-bottom: 20px;
+  }
+`;
+
+export const SubmitButton = styled.button.attrs({
+  type: 'submit',
+})`
+  background: #349beb;
+  color: #fff;
+  border: 0;
+  padding: 0;
+  margin-left: 10px;
+  border-radius: 4px;
+  height: 40px;
+  width: 30%;
+  align-self: center;
+`;
