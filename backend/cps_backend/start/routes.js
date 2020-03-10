ea8b4cb800c53a816931a1c3ed5f5c05e4208892
Vitor <vitormr45@gmail.com>
2020-03-10 14:37:01 -0300

    backend updates

--- backend/cps_backend/start/routes.js
+++ backend/cps_backend/start/routes.js
@@ -1,4 +1,10 @@
 const Route = use('Route');
 
-Route.get('/listcontacts', 'ContactController.index');
 Route.post('/contactus', 'ContactController.store');
+
+Route.post('/adminregister', 'UserController.store');
+
+Route.post('/adminlogin', 'SessionController.store');
+
+Route.get('/listcontacts', 'ContactController.index').middleware(['auth']);
+
